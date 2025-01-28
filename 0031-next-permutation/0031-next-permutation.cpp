dc4fed4c4eda9298f0a9c806c9a66a406cde6dd4
Vaishnavithatikonda24 <vyshnavithatikonda4@gmail.com>
2025-01-28 10:49:53 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.51%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find the pivot
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) { // If pivot is found
+            // Step 2: Find the successor
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
